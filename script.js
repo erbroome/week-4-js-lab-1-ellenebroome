c064ef906fc7478ba19ecf099815d5bae709d653
erbroome <ellenehall@gmail.com>
2025-04-15 15:01:41 -0500

    changes to lab

---
+++ script.js
@@ -0,0 +1,40 @@
+// JavaScript Lab 1 – Problem Solving
+// Write all your code below each prompt
+
+// 1. Sandwich Calculator
+// Each guest gets 2 sandwiches. You have 12 guests.
+// Calculate total sandwiches needed.
+
+let a = 5 
+const a2 = 5
+console.log(a)
+console.log(a2)
+
+
+
+
+
+
+// 2. Movie Budget
+// You have $40. A movie ticket costs $9.50.
+// How many full tickets can you buy, and what's the leftover amount?
+
+
+
+/* 3. Dog Age Converter
+Convert a dog’s age to human years
+(1 dog year = 7 human years)
+*/
+
+
+
+// 4. Combine and Print
+// Create variables for your name, favorite food, and a hobby.
+// Print a sentence using all 3.
+
+
+
+// 🌟 BONUS (Optional)
+// Use prompt() to ask for the user's name, then greet them.
+// let name = prompt("What's your name?");
+// console.log("Hello, " + name + "!");
